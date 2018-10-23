84ca1b50f5eec7b6f646aa4773e8b8cc93d58bbe
Marijan Gradecak <gradecam@tcd.ie>
2018-10-23 18:41:07 +0200

    encapsulate box plot in function

--- Test.R
+++ Test.R
@@ -1,3 +1,8 @@
+library(fitdistrplus)
+library(ggpubr)
+library(dplyr)
+
+
 get_sites <- function(x, y){
   path <- "./results/%s/%s/data/nexus7"  
   full_path <- sprintf(path, x, y)
@@ -17,7 +22,44 @@ read_data <- function(x,y){
   vals <- unlist(lapply(files, read.delim), 
                 recursive=TRUE,
                 use.names=FALSE)
-  return(vals)
+  name <- paste(x,y, sep="_")
+  temp <- data.frame(value=vals, group=name)
+  return(temp)
 }
 
-print(read_data('initial', 'fast_sw'))
+plotTreatment <- function(load, treatments, rm_outlier=FALSE){
+  data <- read_data(load, treatments[1])
+  for(t in treatments[1:length(treatments)]){
+    data <- rbind(data, read_data(load, t))   
+  }
+
+  #Remove the large outlier skewing boxplot
+  if(rm_outlier){
+    OutVals = boxplot(data, plot=FALSE)$out
+    m <- max(sapply(OutVals, max))
+    data <- data[!data$value == m, ]    
+  }
+  
+  group_by(data, group) %>%
+    summarise(
+      count = n(),
+      mean = mean(value, na.rm = TRUE),
+      median = median(value, na.rm = TRUE),
+      sd = sd(value, na.rm = TRUE)
+    )
+  
+  ggboxplot(data, x = "group", y = "value", 
+            color = "group", palette = c("#00AFBB", "#E7B800", "#FC4E07", "#00FF00"),
+            ylab = "Joules", xlab = "Treatment")
+}
+
+plotTreatment('subseq', c('fast_sw', 'fast_nosw'))
+plotTreatment('subseq', c('slow_sw', 'slow_nosw'))
+plotTreatment('initial', c('fast_sw', 'fast_nosw'))
+plotTreatment('initial', c('slow_sw', 'slow_nosw'), rm_outlier=TRUE)
+
+
+
+
+
+
